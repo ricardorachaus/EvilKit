6b285c85f37acb98c892a801543a4d8da57d0525
Ricardo Rachaus <ricardorachaus@gmail.com>
2019-04-29 22:12:37 -0300

    Adding starter project

---
+++ EvilKit/EvilKit/EvilKit.h
@@ -0,0 +1,19 @@
+//
+//  EvilKit.h
+//  EvilKit
+//
+//  Created by Ricardo Rachaus on 29/04/19.
+//  Copyright © 2019 Ricardo Rachaus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EvilKit.
+FOUNDATION_EXPORT double EvilKitVersionNumber;
+
+//! Project version string for EvilKit.
+FOUNDATION_EXPORT const unsigned char EvilKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvilKit/PublicHeader.h>
+
+
